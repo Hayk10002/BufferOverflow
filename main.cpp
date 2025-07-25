db583085e40ceb99cc7e58b857b37f0b34d3044c
Hayk10002 <hayk.g.khachatryan@gmail.com>
2025-07-25 21:03:25 +0400

    Bug fix

--- main.cpp
+++ main.cpp
@@ -5,26 +5,26 @@
 int main(int argc, char* argv[]) {
 
 
-    int __attribute__((no_reoder)) arr_before[4];
+    int __attribute__((no_reorder)) arr_before[4];
     asm volatile("" ::: "memory");
-    int __attribute__((no_reoder)) x = 4321;
+    int __attribute__((no_reorder)) x = 4321;
     asm volatile("" ::: "memory");
-    int __attribute__((no_reoder)) arr_after[4];
+    int __attribute__((no_reorder)) arr_after[4];
 
     int* arr;
-    if ((size_t)&x > (size_t)&arr_before) 
+    if ((size_t)&x > (size_t)arr_before) 
     {
-        std::cout << &x << " > " << &arr_before << std::endl;
+        std::cout << &x << " > " << arr_before << std::endl;
         arr = arr_before;
     }
     else                                
     {
-        std::cout << &x << " < " << &arr_before << std::endl;
+        std::cout << &x << " < " << arr_before << std::endl;
         arr = arr_after;
     } 
     
     std::cout << "&x     : " << &x << std::endl;
-    std::cout << "&arr   : " << &arr << std::endl;
+    std::cout << "&arr[0]: " << arr << std::endl;
     
     int i;
     for (i = 0; x == 4321; i++) 
