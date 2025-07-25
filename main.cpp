85696ce318fbb6be994fe8482ab62822d38092ea
Hayk10002 <hayk.g.khachatryan@gmail.com>
2025-07-25 20:33:28 +0400

    Add support for stacks both growing up and down
    Essentially crossplatform support :)

--- main.cpp
+++ main.cpp
@@ -3,18 +3,17 @@
 
 int main(int argc, char* argv[]) {
 
-    volatile int x = 4321;
-    int arr[4];
 
-    std::cout << "&x    : " << &x << std::endl;
-    std::cout << "&arr  : " << &arr << std::endl;
+    int arr_before[4];
+    volatile int x = 4321;
+    int arr_after[4];
+
+    int* arr;
+    if ((void*)&x > (void*)&arr_before) arr = arr_before;
+    else                                arr = arr_after; 
 
     int i;
-    for (i = 0; x == 4321; i++) 
-    {
-        arr[i] = i;
-        std::cout << "&arr[i]: " << &(arr[i]) << " i=" << i << std::endl;
-    }
+    for (i = 0; x == 4321; i++) arr[i] = i;
     std::cout << i << std::endl;
 
     return 0;
