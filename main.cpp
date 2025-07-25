6bcb87cbfc77dc641040fdc523c91dff1a4b0177
Hayk10002 <hayk.g.khachatryan@gmail.com>
2025-07-25 20:34:57 +0400

    Add back logging

--- main.cpp
+++ main.cpp
@@ -11,9 +11,16 @@ int main(int argc, char* argv[]) {
     int* arr;
     if ((void*)&x > (void*)&arr_before) arr = arr_before;
     else                                arr = arr_after; 
-
+    
+    std::cout << "&x    : " << &x << std::endl;
+    std::cout << "&arr  : " << &arr << std::endl;
+    
     int i;
-    for (i = 0; x == 4321; i++) arr[i] = i;
+    for (i = 0; x == 4321; i++) 
+    {
+        arr[i] = i;
+        std::cout << "&arr[i]: " << &(arr[i]) << " i=" << i << std::endl;
+    }
     std::cout << i << std::endl;
 
     return 0;
