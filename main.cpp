7f12137b42ef86c40f90756ef824cbc70d14f58a
Hayk10002 <hayk.g.khachatryan@gmail.com>
2025-07-25 20:28:30 +0400

    Add some logging

--- main.cpp
+++ main.cpp
@@ -6,8 +6,15 @@ int main(int argc, char* argv[]) {
     volatile int x = 4321;
     int arr[4];
 
+    std::cout << "&x    : " << &x << std::endl;
+    std::cout << "&arr  : " << &arr << std::endl;
+
     int i;
-    for (i = 0; x == 4321; i++) arr[i] = i;
+    for (i = 0; x == 4321; i++) 
+    {
+        arr[i] = i;
+        std::cout << "&arr[i]: " << &(arr[i]) << " i=" << i << std::endl;
+    }
     std::cout << i << std::endl;
 
     return 0;
